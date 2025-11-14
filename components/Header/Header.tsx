b977c13a1bccc338d4ab0ed5784c9f1ae6e8808a
luciostuder <lucio.studer@gmail.com>
2025-11-14 19:05:03 +0000

    aula 1

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <header>
-            <h1>React & Next.js</h1>
+        <header className="flex flex-col items-center">
+            <h1>React ❤️ Next.js</h1>
             <nav className="flex gap-4">
                 <Link href="/" className="hover:underline">Intro</Link>
                 <Link href="/sobre" className="hover:underline">Sobre</Link>
--- components/MagiaDoJSX/MagiaDoJSX.tsx
+++ components/MagiaDoJSX/MagiaDoJSX.tsx
@@ -5,7 +5,7 @@ export default function MagiaDoJSX() {
     const magia = <strong>HTML dentro do JavaScript!</strong>
 
     return (
-        <div>Um compoente é uma função 
+        <div>Um componente é uma função 
             JS que retora JSX - {magia} </div>
     )
 }
