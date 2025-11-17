533dbd730641cb938b4326292fbd2d742d853242
luciostuder <lucio.studer@gmail.com>
2025-11-17 23:53:10 +0000

    botao voltar

--- app/tecnologias/[tecnologia]/page.tsx
+++ app/tecnologias/[tecnologia]/page.tsx
@@ -2,6 +2,7 @@
 
 import { tecnologias } from '@/data/tecnologias'
 import { useParams } from 'next/navigation';
+import Link from 'next/link'
 
 export default function CaracteristicaPage() {
     const params = useParams();
@@ -11,6 +12,7 @@ export default function CaracteristicaPage() {
         <>
             <h2>{tecnologias[id].titulo}</h2>
             <p>{tecnologias[id].descricao}</p>
+            <Link className="mt-8 bg-blue-600 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50" href="/tecnologias">Voltar {'>'}</Link>
         </>
     )
 }
